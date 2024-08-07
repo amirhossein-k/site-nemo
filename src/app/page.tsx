22cff4ecebaa4d08a1efb56e433e096ccf7f82bd
amirhossein-k <amirhossein.k787878@gmail.com>
2024-08-07 21:35:23 +0330

    swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./components/header/Header";
 import Link from "next/link";
-
+import SwiperMain from "./components/swiper/SwiperMain";
+import dataSlider from "./_data/slider-data.json";
 export default function Home() {
   return (
-    <main className="flex min-h-screen ">
+    <main className=" min-h-screen ">
       <Header />
+      <SwiperMain data={dataSlider} />
     </main>
   );
 }
